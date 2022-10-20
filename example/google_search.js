4b62d9717678827d0736f50d63bf19e3f44d577d
Marco Carrozzo <Marco.Carrozzo@hyland.com>
2022-10-20 16:09:26 +0200

    working on chromium tests

--- example/google_search.js
+++ example/google_search.js
@@ -1,14 +1,15 @@
 const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+const { Options } = require('selenium-webdriver/chrome');
 const { elementTextIs } = require('selenium-webdriver/lib/until');
 
 (async function example() {
-
-  var options = new chrome.Options();   
-  options.add_argument('headless')
-  options.add_argument('--disable-infobars')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--remote-debugging-port=9222')
+  
+  var options = new Options();
+  options.addArguments("--headless");
+  options.addArguments('--disable-infobars')
+  options.addArguments('--disable-dev-shm-usage')
+  options.addArguments('--no-sandbox')
+  options.addArguments('--remote-debugging-port=9222')
   
   let driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build();
   try {
