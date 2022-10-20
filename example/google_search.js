deeea60538d5b97586c8f15e850d4f6fee73bddc
Marco Carrozzo <Marco.Carrozzo@hyland.com>
2022-10-20 10:24:11 +0200

    using browser-actions/setup-chrome

--- example/google_search.js
+++ example/google_search.js
@@ -1,12 +1,18 @@
 const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+const { elementTextIs } = require('selenium-webdriver/lib/until');
 
 (async function example() {
   let driver = await new Builder().forBrowser(Browser.CHROMIUM).build();
-
+  try {
     await driver.get('https://www.google.com/ncr');
     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
     await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-
+  } catch (error) {
     await driver.quit();
-
+    console.log(error);
+    process.exit(1);
+  }
+  finally {
+    await driver.quit();
+  }
 })();
